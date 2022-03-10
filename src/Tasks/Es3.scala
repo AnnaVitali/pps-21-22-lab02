5b79d1abab1a9763f5fecb45e238ab1e878a150f
Anna <anna.vitali4@studio.unibo.it>
2022-03-10 19:16:04 +0100

    rewrite Es3.java

--- src/Tasks/Es3.scala
+++ src/Tasks/Es3.scala
@@ -2,10 +2,4 @@ package Tasks
 
 object Es3 extends App:
 
-  val p1: Int => Int => Int => Boolean =  x => y => z => x <= y && y <= z
-  val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && y <= z
-
-  def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y <= z
-  def p4(x: Int, y: Int, z: Int): Boolean = x <= y && y <= z
-
   def compose(f: Int => Int, g: Int => Int): Int => Int = x=> f(g(x))
